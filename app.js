8e628952468506e3cd4108e03060397767e48a80
mohsr <mkrizvi98@gmail.com>
2018-04-17 14:30:12 -0400

    fixed express typo

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'client')));
 
+var bucket = process.env.S3_BUCKET;
+
 // /* Set up AWS for image storage. */
 // var s3 = new aws.S3();
 // var bucket = new aws.S3({
@@ -61,19 +63,54 @@ app.get('/events', function(req, res) {
 });
 
 /* Submit an event. */
-app.post('/submit', function(req, res) {
-	var event = {
-		"event": req.body.event
-	}
-	db.collection('events', function(error, coll) {
-		coll.insert(event, function(error, saved) {
-			if (error) {
-				res.send(500);
-			} else {
-				res.send(200);
-			}
-		});
+// app.post('/submit', function(req, res) {
+// 	var event = {
+// 		"event": req.body.event
+// 	}
+// 	db.collection('events', function(error, coll) {
+// 		coll.insert(event, function(error, saved) {
+// 			if (error) {
+// 				res.send(500);
+// 			} else {
+// 				res.send(200);
+// 			}
+// 		});
+// 	});
+// });
+
+/* AWS testing. */
+app.get('/submit', function(req, res) {
+	res.sendFile('submitformtest.html');
+});
+
+app.get('/storage-get', function(req, res) {
+	const s3 = new aws.S3();
+	const fname = req.query.fname;
+	const ftype = req.query.ftype;
+	const s3params = {
+		Bucket: bucket,
+		Key: fname,
+		Expires: 60,
+		ContentType: ftype,
+		ACL: 'public-read'
+	};
+
+	s3.getSignedUrl('putObject', s3params, function(error, data) {
+		if (error) {
+			res.send(500);
+		} else {
+			const returned = {
+				signedRequest: data,
+				url: ('https://' + bucket + '.s3.amazonaws.com/' + fname)
+			};
+			res.write(JSON.stringify(returned));
+			res.end();
+		}
 	});
+});
+
+app.post('/storage-submit', function(req, res) {
+	res.send('to be done');
 });
 
 /* Listen in on a port. */
