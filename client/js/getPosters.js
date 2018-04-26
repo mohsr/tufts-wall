00e29b37e17b2e26d24bd8524cc1e86540a1c8f5
mohsr <mkrizvi98@gmail.com>
2018-04-26 11:14:37 -0400

    working on image resizing

--- client/js/getPosters.js
+++ client/js/getPosters.js
@@ -16,11 +16,11 @@ function renderEvents(events) {
 	var spot = 'top';
 	for (var i = 0; i < events.length; i++) {
 		var obj = events[i];
-		var newHTML = '<div class="slide' + spot + '"><img src="' +
+		var newHTML = '<div class="img_holder"><div class="slide' + spot + '"><img src="' +
 		              obj.url + '" alt="Event Poster"/><div class="info">' + 
 		              '<p> <span class="eventtitle">' + obj.title + 
 		              '<br/></span>Date: ' + obj.start + ' - ' + obj.end + 
-		              '<br/>' + 'Location: ' + obj.loc + '<br/>' + obj.text + '<br/></p></div></div>';
+		              '<br/>' + 'Location: ' + obj.loc + '<br/>' + obj.text + '<br/></p></div></div></div>';
 		car.append(newHTML);
 		if (spot === 'top') {
 			spot = 'bottom';
