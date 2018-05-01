861054c0696bfbcd3e713c87743aaa2176466c8f
Ralfi Salhon <rifat.salhon@tufts.edu>
2018-04-30 20:26:07 -0400

    test again

--- client/js/getPosters.js
+++ client/js/getPosters.js
@@ -22,17 +22,15 @@ function renderEvents(events) {
 		var parse_date = new Date(Date.parse(obj.date));
 
 		var imgHTML = '<img src="' + obj.url + '" alt="Event Poster"/>';
-		var newHTML = '<div class="slide' + spot + '"><div class="img_holder">' + '</div><div class="info">' + 
-		              '<p> <span class="eventtitle">' + obj.title + 
-		              '<br/></span>Date: ' + obj.date + '<br/>' + 'Time: ' + monthNames[parse_date.getMonth()] + ' '
-		              + parse_date.getDate() + ', ' + parse_date.getFullYear() +
-		              '<br/>' + 'Location: ' + obj.loc + '<br/><br/>' + obj.description +
-					  imgHTML + '<br/></p></div></div>';
-
 		if (obj.page != '') {
 			imgHTML = '<a href="//' + obj.page + '">' + imgHTML + '</a>';
 		}
-		
+		var newHTML = '<div class="slide' + spot + '"><div class="img_holder">' +
+					  imgHTML + '</div><div class="info">' + 
+		              '<p> <span class="eventtitle">' + obj.title + 
+		              '<br/></span>Date: ' + obj.date + '<br/>' + 'Time: ' + monthNames[parse_date.getMonth()] + ' '
+		              + parse_date.getDate() + ', ' + parse_date.getFullYear() +
+		              '<br/>' + 'Location: ' + obj.loc + '<br/><br/>' + obj.description + '<br/></p></div></div>';
 		car.append(newHTML);
 		if (spot === 'top') {
 			spot = 'bottom';
