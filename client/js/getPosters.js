ea6ac9d3137c975262f0298042c87b8d453605bc
Taylor J.. Ampatiellos <Taylor.Ampatiellos@tufts.edu>
2018-04-30 20:20:11 -0400

    Date now displays in intended way?

--- client/js/getPosters.js
+++ client/js/getPosters.js
@@ -15,7 +15,12 @@ function renderEvents(events) {
 	var spot = 'top';
 	for (var i = 0; i < events.length; i++) {
 		var obj = events[i];
+
+		const monthNames = ["January", "February", "March", "April", "May", "June",
+ 							"July", "August", "September", "October", "November", "December"
+						   ];
 		var parse_date = new Date(Date.parse(obj.date));
+
 		var imgHTML = '<img src="' + obj.url + '" alt="Event Poster"/>';
 		if (obj.page != '') {
 			imgHTML = '<a href="' + obj.page + '">' + imgHTML + '</a>';
@@ -23,7 +28,8 @@ function renderEvents(events) {
 		var newHTML = '<div class="slide' + spot + '"><div class="img_holder">' +
 					  imgHTML + '</div><div class="info">' + 
 		              '<p> <span class="eventtitle">' + obj.title + 
-		              '<br/></span>Date: ' + obj.date + '<br/>' + 'Time: ' + parse_date.getMonth() + 
+		              '<br/></span>Date: ' + obj.date + '<br/>' + 'Time: ' + monthNames[parse_date.getMonth()] + ' '
+		              + parse_date.getDate() + ', ' + parse_date.getFullYear() +
 		              '<br/>' + 'Location: ' + obj.loc + '<br/><br/>' + obj.description + '<br/></p></div></div>';
 		car.append(newHTML);
 		if (spot === 'top') {
